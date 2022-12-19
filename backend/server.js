48882ef110e506d84f3fdcd15ab44169d01ed265
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2022-12-19 20:38:09 +0100

    add login pages

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const app = express();
 app.get('/api', (req, res)=>{
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 })
+app.get('/login', (req, res) =>{
+    res.render('login.ejs')
+})
+app.get('/register', (req, res) =>{
+    res.render('register.ejs')
+})
 app.listen(3001, () =>{
     console.log("Hey it works")
 })
