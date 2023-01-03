8c874a934e3b967faf207aec6f165e779dec26d0
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2023-01-03 21:27:11 +0100

    correction App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Form from "./component/Form";
 import Home from "./component/Home";
 import Login from "./component/Login";
 import Register from "./component/Register";
@@ -14,9 +13,8 @@ function App() {
         <Route path="/" element={<Home />}/>
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
-
       </Routes>
-      <Form/>
+
     </Router>
     );
 }
